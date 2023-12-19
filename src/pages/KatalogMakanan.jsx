0c06144cb39e5e4f87395e2ec80a29a8615ea721
NurAzizahMawarAndini <146428367+NurAzizahMawarAndini@users.noreply.github.com>
2023-12-19 03:24:24 -0800

    Update produk

--- src/pages/KatalogMakanan.jsx
+++ src/pages/KatalogMakanan.jsx
@@ -64,14 +64,14 @@ const KatalogMakanan = () => {
            {ProdukTerbaru.slice(0, 1).map((Produk) => {
               return (
                 <Col key={Produk.id} className="rounded">
-                  <img src="../img/Nct.png"alt="unspalsh.com" className=" rounded shadow mt-3" />
-                  <h3 className="fw-bold">Lightstick NCT Dream Korea</h3>
-                  <p style={{ fontSize: '0.8rem' }} >Lampu stik khusus yang melambangkan fandom atau perkumpulan dari fans NCT.</p>
+                  <img src="../img/BlunderCokro.png"alt="unspalsh.com" className=" rounded shadow mt-3" />
+                  <h3 className="fw-bold">Blunder Cokro Khas Madiun</h3>
+                  <p style={{ fontSize: '0.8rem' }} >Produk roti legendaris dengan kelembutan dari roti bluder Cokro memiliki tempat spesial tersendiri di hati.</p>
                   <Row>
                     <Col>
                       <h5 className="fw-bold">
                         <FaCartPlus className="ikoncart-p fa-xl me-2" />
-                        Rp 800.000
+                        Rp 20.000
                       </h5>
                     </Col>
                   </Row>
@@ -83,14 +83,14 @@ const KatalogMakanan = () => {
                       {ProdukTerbaru.slice(0, 1).map((Produk) => {
               return (
                 <Col key={Produk.id} className="rounded">
-                  <img src="../img/Kopi.png" alt="unspalsh.com" className=" rounded shadow mt-3" />
-                  <h3 className="fw-bold" >Chek Hup Coffee (Malaysia)</h3>
-                  <p style={{ fontSize: '0.8rem' }} >Chek Hup Coffee merupakan minuman dari kopi panggang yang nikmat dan memiliki rasa yang khas.</p>
+                  <img src="../img/Bakpia.png" alt="unspalsh.com" className=" rounded shadow mt-3" />
+                  <h3 className="fw-bold" >Bakpia Pathok 25 (Khas Yogyakarta)</h3>
+                  <p style={{ fontSize: '0.8rem' }} >Bakpia Pathok 25 merupakan jajanan populer yang menjadi oleh-oleh khas Kota Yogyakarta.</p>
                   <Row>
                     <Col>
                       <h5 className="fw-bold" >
                         <FaCartPlus className="ikoncart-p fa-xl me-2" />
-                        Rp 10.000
+                        Rp 40.000
                       </h5>
                     </Col>
                   </Row>
@@ -101,14 +101,14 @@ const KatalogMakanan = () => {
             {ProdukTerbaru.slice(0, 1).map((Produk) => {
               return (
                 <Col key={Produk.id} className="rounded">
-                  <img src="../img/Banana.jpg" alt="unspalsh.com" className=" rounded shadow mt-3" />
-                  <h3 className="fw-bold">Banana Chips Rasa Matcha </h3>
-                  <p style={{ fontSize: '0.8rem' }} >Keripik pisang dengan taburan matcha diatasnya dan kaya akan rasa</p>
+                  <img src="../img/StrudelMalang.png" alt="unspalsh.com" className=" rounded shadow mt-3" />
+                  <h3 className="fw-bold">Strudel Oleh - Oleh Khas Malang</h3>
+                  <p style={{ fontSize: '0.8rem' }} >Strudel merupakan kue yang berlapis terbuat dari pastri dan isinya berbagai macam buah-buahan.</p>
                   <Row>
                     <Col>
                       <h5 className="fw-bold">
                         <FaCartPlus className="ikoncart-p fa-xl me-2" />
-                        Rp 10.000
+                        Rp 50.000
                       </h5>
                     </Col>
                   </Row>
@@ -119,14 +119,14 @@ const KatalogMakanan = () => {
            {ProdukTerbaru.slice(0, 1).map((Produk) => {
               return (
                 <Col key={Produk.id} className="rounded">
-                  <img src="../img/Nct.png"alt="unspalsh.com" className=" rounded shadow mt-3" />
-                  <h3 className="fw-bold">Lightstick NCT Dream Korea</h3>
-                  <p style={{ fontSize: '0.8rem' }} >Lampu stik khusus yang melambangkan fandom atau perkumpulan dari fans NCT.</p>
+                  <img src="../img/Lays.png"alt="unspalsh.com" className=" rounded shadow mt-3" />
+                  <h3 className="fw-bold">Lays Fusion (Arab Edition)</h3>
+                  <p style={{ fontSize: '0.8rem' }} >PO dari Arab Saudi, barang sampai akhir oktober. Kami menerima pesanan barang atau makanan.</p>
                   <Row>
                     <Col>
                       <h5 className="fw-bold">
                         <FaCartPlus className="ikoncart-p fa-xl me-2" />
-                        Rp 800.000
+                        Rp 35.000
                       </h5>
                     </Col>
                   </Row>
@@ -138,14 +138,14 @@ const KatalogMakanan = () => {
                       {ProdukTerbaru.slice(0, 1).map((Produk) => {
               return (
                 <Col key={Produk.id} className="rounded">
-                  <img src="../img/Kopi.png" alt="unspalsh.com" className=" rounded shadow mt-3" />
-                  <h3 className="fw-bold" >Chek Hup Coffee (Malaysia)</h3>
-                  <p style={{ fontSize: '0.8rem' }} >Chek Hup Coffee merupakan minuman dari kopi panggang yang nikmat dan memiliki rasa yang khas.</p>
+                  <img src="../img/Piepisang.png" alt="unspalsh.com" className=" rounded shadow mt-3" />
+                  <h3 className="fw-bold" >Pie Pisang Khas Lampung</h3>
+                  <p style={{ fontSize: '0.8rem' }} >Makanan ini memiliki teksturnya kering di bagian luarnya namun bagian dalamnya terasa lembut.</p>
                   <Row>
                     <Col>
                       <h5 className="fw-bold" >
                         <FaCartPlus className="ikoncart-p fa-xl me-2" />
-                        Rp 10.000
+                        Rp 55.000
                       </h5>
                     </Col>
                   </Row>
@@ -156,14 +156,14 @@ const KatalogMakanan = () => {
             {ProdukTerbaru.slice(0, 1).map((Produk) => {
               return (
                 <Col key={Produk.id} className="rounded">
-                  <img src="../img/Banana.jpg" alt="unspalsh.com" className=" rounded shadow mt-3" />
-                  <h3 className="fw-bold">Banana Chips Rasa Matcha </h3>
-                  <p style={{ fontSize: '0.8rem' }} >Keripik pisang dengan taburan matcha diatasnya dan kaya akan rasa</p>
+                  <img src="../img/Dodolgarut.png" alt="unspalsh.com" className=" rounded shadow mt-3" />
+                  <h3 className="fw-bold">Dodol Garut Original</h3>
+                  <p style={{ fontSize: '0.8rem' }} >Dodol Garut memiliki rasa yang manis dan dipadukan dengan berbagai varian rasa.</p>
                   <Row>
                     <Col>
                       <h5 className="fw-bold">
                         <FaCartPlus className="ikoncart-p fa-xl me-2" />
-                        Rp 10.000
+                        Rp 15.000
                       </h5>
                     </Col>
                   </Row>
@@ -174,14 +174,14 @@ const KatalogMakanan = () => {
            {ProdukTerbaru.slice(0, 1).map((Produk) => {
               return (
                 <Col key={Produk.id} className="rounded">
-                  <img src="../img/Nct.png"alt="unspalsh.com" className=" rounded shadow mt-3" />
-                  <h3 className="fw-bold">Lightstick NCT Dream Korea</h3>
-                  <p style={{ fontSize: '0.8rem' }} >Lampu stik khusus yang melambangkan fandom atau perkumpulan dari fans NCT.</p>
+                  <img src="../img/croissant.png"alt="unspalsh.com" className=" rounded shadow mt-3" />
+                  <h3 className="fw-bold">Croissant Arab Saudi</h3>
+                  <p style={{ fontSize: '0.8rem' }} >Croissant termasuk salah satu jenis flaky pastry yang memiliki tekstur berlapis-lapis. </p>
                   <Row>
                     <Col>
                       <h5 className="fw-bold">
                         <FaCartPlus className="ikoncart-p fa-xl me-2" />
-                        Rp 800.000
+                        Rp 15.000
                       </h5>
                     </Col>
                   </Row>
